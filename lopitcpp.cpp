ef50b12fd60b895d561e2759a372d9110a6f7ff5
obsession <97602174+obsessionxd@users.noreply.github.com>
2023-11-14 18:11:04 +0800

    Update lopitcpp.cpp

--- lopitcpp.cpp
+++ lopitcpp.cpp
@@ -1,4 +1,4 @@
-//1:49 am 28/10/2023
+//6:10 pm 14/11/2023
 #include <iostream>
 #include <iomanip>
 #include <cmath>
@@ -11,150 +11,142 @@ using namespace std;
 	string name, address, gender,acctype, birthday;
 	double inidepo, deposit,balance, withdraw;
 	char answer2,answer3,answer4,answer5;
-	 
+	
+	
+void design();
+void design2();
+void deleteacc(double inidepo, double balance);
 int main(int argc, char** argv) {
 do {
-
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-	cout<<"\t\t\t\t\t\t\t\t\t!LOPIT BANKING SYSTEM";
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-	cout<<"\t\t\t\t\t\t\t\t\t1. Open a new account\n\t\t\t\t\t\t\t\t\t2. Balance Inquiry\n\t\t\t\t\t\t\t\t\t3. Deposit\n\t\t\t\t\t\t\t\t\t4. Withdraw\n\t\t\t\t\t\t\t\t\t5. View Account Information\n\t\t\t\t\t\t\t\t\t6. Close Account\n\t\t\t\t\t\t\t\t\t7. Exit Program\n";
-	cout<<"\t\t\t\t\t\t\t\t\t ";
+	design();
+	cout<<"1. Open a new account\n2. Balance Inquiry\n3. Deposit\n4. Withdraw\n5. View Account Information\n6. Close Account\n7. Exit Program\n";
+	cout<<"";
 	cin>>answer;
 	switch(answer){	
 	case 1: 
 	system("cls");
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-	cout<<"\t\t\t\t\t\t\t\t\t!LOPIT BANKING SYSTEM";
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-	cout<<"\n\t\t\t\t\t\t\t\t\tOpening new account";
-	cout<<"\n\n\t\t\t\t\t\t\t\tEnter full name(FN, LN, MN): ";
+	design();
+	cout<<"\nOpening new account";
+	cout<<"\n\nEnter full name(FN, LN, MN): ";
 	cin.ignore();
 	getline(cin, name);
-	cout<<"\n\t\t\t\t\t\t\t\tAddress: ";
+	cout<<"\nAddress: ";
 	getline(cin, address);
-	cout<<"\n\t\t\t\t\t\t\t-------------------------------------------------------------\n";
-	cout<<"\n\t\t\t\t\t\t\t\t\t\tCurrent Date\n";
-	cout<<"\n\t\t\t\t\t\t\t\tEnter Day today(1 - 31): ";
+	design2();
+	cout<<"\nCurrent Date\n";
+	cout<<"\nEnter Day today(1 - 31): ";
 	cin>>day;
 	if (day <=0 || day > 31) {       // checks if the inputted current date is valid 
 		system("cls");
-		cout<<"\n\t\t\t\t\t\t\t\t\tInvalid Day.";
+		cout<<"\nInvalid Day.";
 	}
 	else {
-		cout<<"\n\t\t\t\t\t\t\t\tEnter Month today(1 - 12): ";
+		cout<<"\nEnter Month today(1 - 12): ";
 	cin>>month;
 	if(month <=0 ||month > 12) {  // checks if inputted current month is valid
 		system("cls");
-		cout<<"\n\t\t\t\t\t\t\t\t\tInvalid Month.";
+		cout<<"\nInvalid Month.";
 		 }
 		 else { 
-		 	cout<<"\n\t\t\t\t\t\t\t\tEnter Year today: ";  
+		 	cout<<"\nEnter Year today: ";  
 	        cin>>year;
-	        cout<<"\n\t\t\t\t\t\t\t\tThe date today is "<<day<<"/"<<month<<"/"<<year<<".";
-		cout<<"\n\t\t\t\t\t\t\t-------------------------------------------------------------\n";
-	cout<<"\n\n\t\t\t\t\t\t\t\t\t\tBirth Date\n";
-		cout<<"\n\t\t\t\t\t\t\t\tEnter birth day (1-31): ";
+	        cout<<"\nThe date today is "<<day<<"/"<<month<<"/"<<year<<".";
+		design2();
+	cout<<"\n\nBirth Date\n";
+		cout<<"\nEnter birth day (1-31): ";
 		cin>>bday;
 		if(bday<=0 ||bday > 31) { //checks if inputted birth day is valid
 		system("cls");
-		cout<<"\n\t\t\t\t\t\t\t\tInvalid birth day, choose from 1-31 only.";
+		cout<<"\nInvalid birth day, choose from 1-31 only.";
 		}
 		else {
-		cout<<"\n\t\t\t\t\t\t\t\tEnter birth month (1-12): ";
+		cout<<"\nEnter birth month (1-12): ";
 		cin>>bmonth;
 		if(bmonth <=0 ||bmonth > 12) {
 		system("cls");
-		cout<<"\n\t\t\t\t\t\t\t\tInvalid birth month, choose from 1-12 only.";
+		cout<<"\nInvalid birth month, choose from 1-12 only.";
 		}
 		else { 
-		cout<<"\n\t\t\t\t\t\t\t\tEnter Birth year: ";
+		cout<<"\nEnter Birth year: ";
 		cin>>byear;
 		age = year - byear;
 		if (month < bmonth ||month == bmonth && day < bday) {
 		age--;	
 		}
-		cout<<"\n\t\t\t\t\t\t\t\tYour birthday is "<<bday<<"/"<<bmonth<<"/"<<byear<<".";
+		cout<<"\nYour birthday is "<<bday<<"/"<<bmonth<<"/"<<byear<<".";
 		
 		if (age >= 18) {
-	    cout<<"\n\t\t\t\t\t\t\t-------------------------------------------------------------\n";
-     	cout<<"\t\t\t\t\t\t\t\t\t\tGender\n";
-			cout<<"\n\n\t\t\t\t\t\t\t\tGender(M or F): ";
+	    design2();
+     	cout<<"Gender\n";
+			cout<<"\n\nGender(M or F): ";
 			cin>>gender;
 			while (gender != "M" && gender != "m" && gender!= "F" && gender != "f") {
 			system("cls");
-			cout<<"\n\n\t\t\t\t\t\t\t\tInvalid Gender, choose M or F only: ";
+			cout<<"\n\nInvalid Gender, choose M or F only: ";
 			cin>>gender;		
 			}
 			
 			if (gender == "M" || gender == "m") {
 				gender = "Male";
-				cout<<"\t\t\t\t\t\t\t\tGender is "<<gender;
+				cout<<"Gender is "<<gender;
 			}
 				else if (gender == "F" || gender == "f") {
 				gender = "Female";
-				cout<<"\t\t\t\t\t\t\t\tGender is "<<gender;
+				cout<<"Gender is "<<gender;
 			}	
-				cout<<"\n\t\t\t\t\t\t\t-------------------------------------------------------------\n";
-     	cout<<"\t\t\t\t\t\t\t\t\t\tAccount Type\n";
-		    cout<<"\n\n\t\t\t\t\t\t\t\tAccount Type(S for Savings, C for Current): ";
+				design2();
+     	cout<<"Account Type\n";
+		    cout<<"\n\nAccount Type(S for Savings, C for Current): ";
 		    cin>>acctype;
 		while(acctype!= "S" && acctype!= "s" && acctype!= "C" &&  acctype!= "c") {
-			cout<<"\n\n\t\t\t\t\t\t\t\tInvalid account type, choose from Savings(S) and Current(C) only: ";
+			cout<<"\n\nInvalid account type, choose from Savings(S) and Current(C) only: ";
 		    cin>>acctype;
 		}
 		
 		if (acctype == "S"|| acctype == "s") {
 			acctype = "Savings";
-			cout<<"\t\t\t\t\t\t\t\tAccount type is "<<acctype<<" account";
-			cout<<"\n\t\t\t\t\t\t\t-------------------------------------------------------------\n";
-     	cout<<"\n\t\t\t\t\t\t\t\t\t\tInitial Deposit\n";
-			cout<<"\n\t\t\t\t\t\t\t\tInitial Deposit(Minimum of 5000): ";
+			cout<<"Account type is "<<acctype<<" account";
+			design2();
+     	cout<<"\nInitial Deposit\n";
+			cout<<"\nInitial Deposit(Minimum of 5000): ";
 				cin>>inidepo;
 				balance += inidepo;
 		
 			while (inidepo < 5000) {
-				cout<<"\n\n\t\t\t\t\t\t\t\tInitial deposit is not sufficient, reenter: ";
+				cout<<"\n\nInitial deposit is not sufficient, reenter: ";
 				cin>>inidepo;
 				balance += inidepo;
 			}
 		}
 		else if (acctype == "C" || acctype == "c") {
 			acctype = "Current";
-			cout<<"\n\t\t\t\t\t\t\t\tAccount type is "<<acctype<<" account";
-			 cout<<"\n\t\t\t\t\t\t\t-------------------------------------------------------------\n";
-     	cout<<"\n\t\t\t\t\t\t\t\t\t\tInitial Deposit\n";
-				cout<<"\n\n\t\t\t\t\t\t\t\tInitial Deposit(Minimum of 10000): ";
+			cout<<"\nAccount type is "<<acctype<<" account";
+			 design2();
+     	cout<<"\nInitial Deposit\n";
+				cout<<"\n\nInitial Deposit(Minimum of 10000): ";
 				cin>>inidepo;
 			while (inidepo < 10000) {
-				cout<<"\n\t\t\t\t\t\t\t\tInitial deposit is not sufficient, reenter: ";
+				cout<<"\nInitial deposit is not sufficient, reenter: ";
 				cin>>inidepo;
 			}		
 		}
-            	cout<<"\n\n\t\t\t\t\t\t\t\tEnter PIN(6 digits only): ";
+            	cout<<"\n\nEnter PIN(6 digits only): ";
             	cin>>pin;
             	while (pin > 999999) {
-            		cout<<"\n\t\t\t\t\t\t\t\tEnter a 6 digit number only";
+            		cout<<"\nEnter a 6 digit number only";
             		cin>>pin;
 				}
             	
-				cout<<"\n\t\t\t\t\t\t\t\tAccount Successfully Created!";
+				cout<<"\nAccount Successfully Created!";
 			    srand(time(0));
 				for (int i=0; i<1; i++) {
-					cout<<"\n\t\t\t\t\t\t\t\tAccount number: "<<rand()<<""<<endl;		
+					cout<<"\nAccount number: "<<rand()<<""<<endl;		
 				}
 			
-				
-			
-				
-			
-			
-			
-				
 		}
 		else {
 			system("cls");
-			cout<<"\n\t\t\t\t\t\t\t\tYou are too young to open an account.\n\n";
+			cout<<"\nYou are too young to open an account.\n\n";
 		}
 		}
 	
@@ -169,60 +161,56 @@ do {
 	break;
 	case 2: 
 	system("cls");
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-	cout<<"\t\t\t\t\t\t\t\t\t!LOPIT BANKING SYSTEM";
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-	cout<<"\t\t\t\t\t\t\t\t\tBalance Inquiry\n";
+	design();
+	cout<<"Balance Inquiry\n";
 	if (inidepo > 0) {
-		cout<<"\t\t\t\t\t\t\t\t\tEnter password: ";
+		cout<<"Enter password: ";
 		cin >>password;
 	
 	while (password != pin) {
-		cout<<"\t\t\t\t\t\t\t\t\tWrong password, enter again: ";
+		cout<<"Wrong password, enter again: ";
 		cin >>password;	
 	}
-		cout<<"\t\t\t\t\t\t\t\t\tBalance: "<<balance<<endl;	  
+		cout<<"Balance: "<<balance<<endl;	  
 	}
 	else {
-		cout<<"\t\t\t\t\t\t\t\t\tCreate an account first.";
+		cout<<"Create an account first.";
 	}
 	break;
 	
 	//option 3
 	case 3: 
 	system("cls");
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-	cout<<"\t\t\t\t\t\t\t\t\t!LOPIT BANKING SYSTEM";
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-	cout<<"\t\t\t\t\t\t\t\t\tDeposit\n";
+	design();
+	cout<<"Deposit\n";
 	if (inidepo > 0) {
-		cout<<"\t\t\t\t\t\t\t\t\tEnter PIN: ";
+		cout<<"Enter PIN: ";
 		cin>>password2;
 		while (pin != password2 ) {
-		cout<<"\n\t\t\t\t\t\t\t\t\tWrong password, enter again: ";
+		cout<<"\nWrong password, enter again: ";
 		cin>>password2;
 	}
 		do {
-	cout<<"\n\t\t\t\t\t\t\t\t\tHow much do you want to deposit?: ";
+	cout<<"\nHow much do you want to deposit?: ";
 	cin>>deposit;
 	if (acctype == "Savings") {
 		while (deposit < 300 || deposit < 0) {
-			cout<<"\t\t\t\t\t\t\t\t\tEntered amount is not valid, try again: ";
+			cout<<"Entered amount is not valid, try again: ";
 			cin>>deposit;
 		}
 	}
 	if (acctype == "Current") {
 		while (deposit < 500 || deposit < 0) {
-			cout<<"\t\t\t\t\t\t\t\t\tEntered amount is not valid, try again: ";
+			cout<<"Entered amount is not valid, try again: ";
 			cin>>deposit;
 		}
 	}
-		cout<<"\t\t\t\t\t\t\t\t\tConfirm deposit of "<<deposit<<" ?(Y/N)";
+		cout<<"Confirm deposit of "<<deposit<<" ?(Y/N)";
 	cin>>answer2;
 	if (answer2 == 'Y' || answer2 =='y') {
-			cout<<"\t\t\t\t\t\t\t\t\tSuccessfully deposited "<<deposit;
+			cout<<"Successfully deposited "<<deposit;
 			balance += deposit;
-			cout<<"\n\t\t\t\t\t\t\t\t\tNew Balance: "<<balance;
+			cout<<"\nNew Balance: "<<balance;
 	}
 }
 
@@ -230,45 +218,41 @@ while (answer2 != 'Y' || answer2 == 'y');
 	}
 	
 	else {
-		cout<<"\t\t\t\t\t\t\t\t\tCreate an account first.";
+		cout<<"Create an account first.";
 	}
 	break;
 	
 	// option number 4
 	case 4: 
 	system("cls");
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-	cout<<"\t\t\t\t\t\t\t\t\t!LOPIT BANKING SYSTEM";
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-	cout<<"\t\t\t\t\t\t\t\t\tWithdraw\n";
+	design();
+	cout<<"Withdraw\n";
 	if (inidepo <= 0) {
-	cout<<"\t\t\t\t\t\t\t\t\tCreate an account first.";	
+	cout<<"Create an account first.";	
 	}
 	else {
-		cout<<"\t\t\t\t\t\t\t\t\tEnter PIN: ";
+		cout<<"Enter PIN: ";
 		cin>>password3;	
 		while (pin!= password3) {
-		   cout<<"\t\t\t\t\t\t\t\t\tWrong password, enter again: ";
+		   cout<<"Wrong password, enter again: ";
 		   cin>>password3;	
 		}
 		do {
-		cout<<"\n\t\t\t\t\t\t\t\t\tHow much do you want to withdraw?: ";
+		cout<<"\nHow much do you want to withdraw?: ";
 	    cin>>withdraw;
 	    while (withdraw <= 0 || withdraw > balance) {
-		cout<<"\n\t\t\t\t\t\t\t\t\tInvalid amount, try again: ";
+		cout<<"\nInvalid amount, try again: ";
 		cin>>withdraw;
  	           		                                        }
- 	    cout<<"\n\t\t\t\t\t\t\t\t\tConfirm withdrawal of "<<withdraw<<" ?";
+ 	    cout<<"\nConfirm withdrawal of "<<withdraw<<" ?";
  	    cin>>answer3;
  	    
  	    if (answer3 == 'Y' || answer3 == 'Y' ) {
  	          system("cls");
- 	          	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-              	cout<<"\t\t\t\t\t\t\t\t\t!LOPIT BANKING SYSTEM";
-	            cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
- 	    	    cout<<"\n\t\t\t\t\t\t\t\t\tSuccessfully withdrew "<<withdraw;
+ 	          	design();
+ 	    	    cout<<"\nSuccessfully withdrew "<<withdraw;
  	    	    balance -= withdraw;
- 	    	    cout<<"\n\t\t\t\t\t\t\t\t\tNew Balance:  "<<balance;
+ 	    	    cout<<"\nNew Balance:  "<<balance;
 		 }
 		}
 		while (answer3 =='N' || answer3 == 'n');
@@ -279,28 +263,26 @@ while (answer2 != 'Y' || answer2 == 'y');
 	//option number 5
 	case 5: 
 	system("cls");
-		cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-        cout<<"\t\t\t\t\t\t\t\t\t!LOPIT BANKING SYSTEM";
-	    cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
+		design();
 	    if (inidepo > 0) {
-	    		cout<<"\t\t\t\t\t\t\t\t\t\tAccount Information\n";
-	    		cout<<"\n\t\t\t\t\t\t\t\t\t\t Enter PIN: ";
+	    		cout<<"Account Information\n";
+	    		cout<<"\nEnter PIN: ";
 	    		cin>>password4;
 	    		do {
-	    		cout<<"\n\t\t\t\t\t\t\t\t\t\t Wrong password, enter again: ";
+	    		cout<<"\nWrong password, enter again: ";
 	    		cin>>password4;	
 				}
 				while (password4 != pin);
 	    		
-	cout<<"\n\t\t\t\t\t\t\t\t\tFull Name: "<<name;
-	cout<<"\n\t\t\t\t\t\t\t\t\tBirthday: "<<bday<<"/"<<bmonth<<"/"<<byear;
-	cout<<"\n\t\t\t\t\t\t\t\t\tGender: "<<gender;
-	cout<<"\n\t\t\t\t\t\t\t\t\tAccount Type: "<<acctype;
-	cout<<"\n\t\t\t\t\t\t\t\t\tInitial Deposit: "<<inidepo;
-	cout<<"\n\t\t\t\t\t\t\t\t\tCurrent Balance: "<<balance;
+	cout<<"\nFull Name: "<<name;
+	cout<<"\nBirthday: "<<bday<<"/"<<bmonth<<"/"<<byear;
+	cout<<"\nGender: "<<gender;
+	cout<<"\nAccount Type: "<<acctype;
+	cout<<"\nInitial Deposit: "<<inidepo;
+	cout<<"\nCurrent Balance: "<<balance;
 		}
 		else {
-	cout<<"\t\t\t\t\t\t\t\t\tNo account found.\n\n";
+	cout<<"No account found.\n\n";
 		}
 	
 	break;
@@ -308,49 +290,64 @@ while (answer2 != 'Y' || answer2 == 'y');
 	//option number 6
 	case 6: 
 	system("cls");
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
-    cout<<"\t\t\t\t\t\t\t\t\t!LOPIT BANKING SYSTEM";
-	cout<<"\n\t\t\t\t\t\t----------------------------------------------------------------------------\n";
+	design();
 	if (inidepo > 0) {
-		cout<<"\t\t\t\t\t\t\t\t\t\tEnter PIN: ";
+		cout<<"Enter PIN: ";
 	cin>>password5;
 	
 	do {
-	cout<<"\t\t\t\t\t\t\t\t\t\tWrong password, enter again: ";
+	cout<<"Wrong password, enter again: ";
 	cin>>password5;
 	}
 	while(password5 != pin);
 	
 	
-	cout<<"\t\t\t\t\t\t\t\t\t\tDo you really want to close the account?(Y/N): ";
+	cout<<"Do you really want to close the account?(Y/N): ";
 	cin>>answer4;
 	if (answer4 == 'Y') {
-		inidepo = 0;
-		balance = 0;
-		cout<<"\t\t\t\t\t\t\t\t\t\tThe account is now closed.";
+		deleteacc(inidepo,balance);
 	}
 	
 	}
 	else {
-		cout<<"\t\t\t\t\t\t\t\t\t\tCreate an account first: ";
+		cout<<"Create an account first: ";
 	}
 	
 	while (answer4 =='N');
 	break;
+	
+	case 7: 
+	cout<<"\nDo you really want to end the program? (Y\N): ";
+	cin>>answer5;
+	break;
 
 	}
-
 }
-	while (answer!=7);
-		cout<<"\t\t\t\t\t\t\t\t\t\tDo you really want to end the program?";
-	cin>>answer5;
+while (answer5 != 'Y' && answer5!= 'y');
+        system("cls");
+		cout<<"\nThank you for using our !LOPIT Online Banking.\n";
+		cout<<"\nMariño, Carl Jerome N.";
+		cout<<"\nMark Gamboa";
+		cout<<"\nRonn Rosarito";
+	    cout<<"\nDominic Madlangbayan";
 	
-		cout<<"\n\t\t\t\t\t\t\t\t\tThank you for using our program.";
-		cout<<"\n\t\t\t\t\t\t\t\t\tMariño, Carl Jerome N.";
-		cout<<"\n\t\t\t\t\t\t\t\t\tMark Gamboa";
-		cout<<"\n\t\t\t\t\t\t\t\t\tRonn Rosarito";
-	    cout<<"\n\t\t\t\t\t\t\t\t\tDominic Madlangbayan";
-		
+	
 	
 	return 0;
+}
+
+
+void deleteacc(double, double) {
+	inidepo = 0;
+	balance = 0;
+	cout<<"The account is now closed.";
+}
+void design(){
+	cout<<"\n------------------------------------\n";
+	cout<<"\t!LOPIT  SYSTEM";
+	cout<<"\n------------------------------------\n";
+}
+void design2(){
+	cout<<"\n------------------------------------\n";
+
 }
